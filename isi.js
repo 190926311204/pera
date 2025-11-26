eab0a9bdab593e3bbef4380879868e6bee1a2c7c
pramlya <peraamelia48@gmail.com>
2025-11-26 13:01:08 +0700

    Add files via upload

---
+++ isi.js
@@ -0,0 +1,7 @@
+document.writeln("<center>");
+document.writeln("<hr width=600 color=pink>");
+document.writeln("<h1> SMK AL GHIFFARI BANYURESMI</h1>");
+document.writeln("<h2> KAREES GARUT<h2>");
+document.writeln("<h3> JURUSAN: rekayasa perangkat lunak <h3>");
+document.writeln("<hr width =600 color=pink>");
+document.writeln("</center");
